fb8173629e01af87e1839169642b9930bc83cd96
TM <tjmcard@gmail.com>
2024-01-02 11:55:04 -0800

    Default Location Client

---
+++ app/src/main/java/com/abyxcz/mad_locations/LocationClient.kt
@@ -0,0 +1,10 @@
+package com.abyxcz.mad_locations
+
+import android.location.Location
+import kotlinx.coroutines.flow.Flow
+
+interface LocationClient {
+    fun getLocationUpdates(interval: Long): Flow<Location>
+
+    class LocationException(message: String): Exception()
+}
