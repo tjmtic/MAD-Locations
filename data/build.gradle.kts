18c8f0bd509ad8abb781410e5980e5f15a1d8225
TM <tjmcard@gmail.com>
2024-01-09 12:13:32 -0800

    Add Hilt Dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -36,7 +38,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
@@ -53,4 +55,9 @@ dependencies {
 
     //Encryption
     implementation("net.zetetic:android-database-sqlcipher:4.5.0")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-compiler:2.48.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 }
