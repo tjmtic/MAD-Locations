41f614cd4a838339bca72dc6599d2f2702347a79
TM <tjmcard@gmail.com>
2024-01-09 12:14:41 -0800

    Create FrameworkModule.kt

---
+++ app/src/main/java/com/abyxcz/mad_locations/di/FrameworkModule.kt
@@ -0,0 +1,20 @@
+package com.abyxcz.mad_locations.di
+
+import android.content.Context
+import com.abyxcz.mad_locations.DefaultLocationClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FrameworkModule {
+    @Provides
+    fun provideDefaultLocationClient(@ApplicationContext context: Context): DefaultLocationClient {
+        return DefaultLocationClient(context, FusedLocationProviderClient(context))
+    }
+}
