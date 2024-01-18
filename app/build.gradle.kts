e8e5f46dd0b82dcc429a0b5732fab8d972847b07
TM <tjmcard@gmail.com>
2024-01-17 20:31:05 -0800

    Update coroutine dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-compiler:2.48.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 }
 
 
